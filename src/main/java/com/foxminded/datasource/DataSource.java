a14ac81dd214e910cf4bde4c49f0d8885aeb2bbc
Yura Odnorog <odnorog.yu@gmail.com>
2021-12-18 02:49:28 +0200

    refactor

--- src/main/java/com/foxminded/datasource/DataSource.java
+++ src/main/java/com/foxminded/datasource/DataSource.java
@@ -1,7 +1,6 @@
 package com.foxminded.datasource;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,27 +9,24 @@ import java.util.Properties;
 
 public class DataSource {
 
-    private static Connection connection = null;
-    private static FileInputStream fileInputStream = null;
+    private static final String driverSQL = "org.postgresql.Driver";
 
-    public Connection getConnection () throws ClassNotFoundException, IOException {
-        Class.forName("org.postgresql.Driver");
+    static {
         try {
-            fileInputStream = new FileInputStream("src/main/resources/config.properties");
+            Class.forName(driverSQL);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection () throws IOException, SQLException {
+        try (FileInputStream fileInputStream = new FileInputStream("src/main/resources/config.properties")){
             Properties properties = new Properties();
             properties.load(fileInputStream);
             String host = properties.getProperty("db.host");
             String login = properties.getProperty("db.login");
             String pass = properties.getProperty("db.password");
-            connection = DriverManager.getConnection(host,login,pass);
-            return connection;
-        }catch (SQLException | FileNotFoundException e){
-            System.out.println("Connection Failed");
-            e.printStackTrace();
-            return connection;
-        }finally {
-            assert fileInputStream != null;
-            fileInputStream.close();
+            return DriverManager.getConnection(host,login,pass);
         }
     }
 
--- src/main/java/com/foxminded/school/School.java
+++ src/main/java/com/foxminded/school/School.java
@@ -4,14 +4,15 @@ import com.foxminded.datasource.DataSource;
 import com.foxminded.randomdata.RandomData;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 
 public class School {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, SQLException {
         DataSource dataSource = new DataSource();
         dataSource.getConnection();
         RandomData randomData = new RandomData();
-        System.out.println(randomData.random());
+        System.out.println(randomData.randomGroups());
 
     }
 }
