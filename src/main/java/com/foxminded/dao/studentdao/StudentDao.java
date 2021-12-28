8cdf1d9791494a5fbd2551c1b81cfcd0381daf6a
Yura Odnorog <odnorog.yu@gmail.com>
2021-12-28 21:38:21 +0200

    refactoring and add test db

---
+++ src/main/java/com/foxminded/dao/studentdao/StudentDao.java
@@ -0,0 +1,14 @@
+package com.foxminded.dao.studentdao;
+
+import com.foxminded.student.Student;
+
+import java.util.List;
+
+public interface StudentDao {
+
+        void create (String first_name , String last_name , int group_id);
+        List<Student> findAll();
+        Student findId(int student_id);
+        void update (int group_id ,String  first_name ,String last_name , int student_id);
+        void delete (int student_id );
+}
